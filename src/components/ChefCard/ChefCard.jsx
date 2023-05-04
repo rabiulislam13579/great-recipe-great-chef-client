c612a51aa1277a473ab3a0b796859d3f0eef54e6
rabiulislam13579 <rabiul120158@gmail.com>
2023-05-04 20:38:16 +0600

    chefCard

---
+++ src/components/ChefCard/ChefCard.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import SingleChef from './SingleChef';
+
+const ChefCard = () => {
+    const [chef,setChef]=useState([])
+    useEffect(()=>{
+        fetch('https://great-recipe-great-chef-server-rabiulislam13579.vercel.app/allchef')
+        .then(res=>res.json())
+        .then(data=>setChef(data))
+        
+    },[])
+   
+
+    return (
+        <div className='c-card container mb-3'>
+            {
+                chef.map(singleChef=><SingleChef
+                key={singleChef.id}
+                singleChef={singleChef}
+                ></SingleChef>)
+            }
+        </div>
+    );
+};
+
+export default ChefCard;
